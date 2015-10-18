c237f16f8948657cfdc2bd080934c031e59d1b25
ankjai <ankit.jaiswal@appdirect.com>
2015-10-17 20:45:48 -0700

    Adding grunt pagespeed and ngrok support.

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+'use strict'
+
+var ngrok = require('ngrok');
+
+module.exports = function(grunt) {
+
+    // Load grunt tasks
+    require('load-grunt-tasks')(grunt);
+
+    // Grunt configuration
+    grunt.initConfig({
+        pagespeed: {
+            options: {
+                nokey: true,
+                locale: "en_GB",
+                threshold: 40
+            },
+            local: {
+                options: {
+                    url: "http://1b44ccf0.ngrok.io",
+                    strategy: "desktop",
+                    threshold: 90
+                }
+            },
+            mobile: {
+                options: {
+                    url: "http://1b44ccf0.ngrok.io",
+                    strategy: "mobile",
+                    threshold: 90
+                }
+            }
+        }
+    });
+
+    // Register customer task for ngrok
+    grunt.registerTask('psi-ngrok', 'Run pagespeed with ngrok', function() {
+        var done = this.async();
+        var port = 8000;
+        // var url;
+
+        ngrok.connect(port, function(err, url) {
+            if (err !== null) {
+                grunt.fail.fatal(err);
+                return done();
+            }
+            grunt.config.set('pagespeed.options.url', url);
+            grunt.task.run('pagespeed');
+            done();
+        });
+    });
+
+    // Register default tasks
+    grunt.registerTask('default', ['psi-ngrok']);
+}
